f9a3d5bf1251a58df25b9491c083408af3a25f85
binarytester <110983840+binarytester@users.noreply.github.com>
2023-02-16 19:29:02 -0300

    Update dictController.js

--- dict/dictController.js
+++ dict/dictController.js
@@ -5,6 +5,7 @@ const dictOp = require("./operationsDict");
 // router to save the trasncription of word
 router.post("/text/save", (req, res) => {
   var text = req.body.text.toLowerCase();
+  dictOp.generateAudio(text);
   text = dictOp.findWord(text);
   var engWord = dictOp.translateWord(text);
   text = res.render("../views/transcripited/newText.ejs", {
