ccb3b7b5f12ae543204ddc2464967a4b92af4bc8
binarytester <110983840+binarytester@users.noreply.github.com>
2023-02-22 20:43:55 -0300

    Create index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const dictController = require("./dict/dictController");
 const app = express();
 const bodyParser = require("body-parser");
+
+const dictOp = require("./dict/operationsDict");
+const gAudio = require("./dict/audioGenerator");
 
 // define view engine to ejs
 app.set("view engine", "ejs");
@@ -11,11 +13,25 @@ app.use(bodyParser.json());
 // define path of static files
 app.use(express.static("public"));
 
-// use file in index
-app.use("/", dictController);
-
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+// router to save the transcription of word
+app.post("/text/save", (req, res) => {
+  var text = req.body.text.toLowerCase();
+
+  // generate audio
+  gAudio.generateAudio(text);
+  // traslating IPA to words
+  text = dictOp.findWord(text);
+  var engWord = dictOp.translateWord(text);
+
+  res.render("../views/transcripited/newText.ejs", {
+    text: text,
+    audioSrc: "../audio.mp3",
+    engWord: engWord,
+  });
 });
 
 app.listen(process.env.PORT || 3000, () => {
